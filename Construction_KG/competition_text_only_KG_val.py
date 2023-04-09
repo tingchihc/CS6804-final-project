95636bb0030b94c534df67429376934425e785a2
Mike@0216 <91083830+ting-chih@users.noreply.github.com>
2023-04-09 16:05:53 -0400

    Add files via upload

---
+++ Construction_KG/competition_text_only_KG_val.py
@@ -0,0 +1,104 @@
+import os
+import json
+import networkx as nx
+import pickle
+import matplotlib.pyplot as plt
+
+def build_up_nx_KG(J_FILE, split_set):
+    
+    with open(J_FILE) as jj:
+        data = json.load(jj)
+
+    for c in range(0, len(data['data'])):
+        #print(data['data'][c]['page_ids'], data['data'][c]['questionId'])
+        extract_info_from_preprocess_dataset(data['data'][c]['page_ids'], data['data'][c]['questionId'], split_set)
+
+def extract_info_from_preprocess_dataset(pids, QID, split_set):
+    
+    node = {} #{page_id: [n1,n2,n3,...], ...}
+    
+    for p in pids:
+        page_number = p.split('_')[1]
+        node[page_number] = []
+        info = preprocess_dataset + p + '/info.json'
+        ocr = '/home/grads/tingchih/dataset/DocVQA_task4/ocr/' + p + '.json'
+        
+        if os.path.exists(info) == True:
+
+            with open(ocr) as jj:
+                ocr_data = json.load(jj)
+            
+            with open(info) as jj:
+                info_data = json.load(jj)
+        
+            for k, v in info_data.items():
+                if int(k) < len(ocr_data['LINE']):
+                    node[page_number].append(v['Text'])
+    
+        print('page_number: ', page_number)
+
+    generate_text_only_KG(node, QID, split_set)
+
+def generate_text_only_KG(node_dict, qid, split_set):
+    
+    G = nx.Graph()
+    G.clear()
+    page_keys = list(node_dict.keys())
+    pk = []
+    for p in page_keys:
+        pk.append((p, {"color": "red"}))
+    G.add_nodes_from(pk, info='page_number')
+    
+    # page nodes link with page nodes
+    for pk in range(0, len(page_keys)):
+        if page_keys[pk] != page_keys[-1]:
+            G.add_edge(page_keys[pk], page_keys[pk+1])
+            
+    
+    for k, v in node_dict.items():
+        G.add_nodes_from(v, color='blue')
+    
+    edges = {} # {0:[(n1,n2), (n2,n3)], 1:[]}
+    
+    for k, v in node_dict.items():
+        edges[k] = []
+        for vv in range(0, len(v)):
+            # nodes link with page node
+            edges[k].append((k,v[vv]))
+            # nodes link with other nodes
+            if v[vv] != v[-1]:
+                edges[k].append((v[vv], v[vv+1]))
+    
+    for ek, ev in edges.items():
+        G.add_edges_from(ev)
+    
+    # save network as pickle file
+    filename = '/home/grads/tingchih/dataset/DocVQA_task4/competition_dataset/text_only_KG/' + split_set + '/' + str(qid) + '.p'
+    with open(filename, 'wb') as f:
+        pickle.dump(G, f)
+    
+    print('[FINISH] ', split_set, str(qid))
+
+    try:
+        # visulize_KG
+        V_filename = '/home/grads/tingchih/dataset/DocVQA_task4/competition_dataset/text_only_KG/graph_visulization/' + str(qid) + '.png'
+        plt.figure(3,figsize=(12,12))
+        node_colors = [node[1]['color'] for node in G.nodes(data=True)]
+        nx.draw(G, with_labels = True, node_size=90,font_size=8, node_color=node_colors)
+        plt.savefig(V_filename)
+        plt.clf()
+        print('[VISULIZE] ', V_filename)
+    except:
+        return 0
+
+
+
+train_root = '/home/grads/tingchih/dataset/DocVQA_task4/competition_dataset/train.json'
+val_root = '/home/grads/tingchih/dataset/DocVQA_task4/competition_dataset/val.json'
+test_root = '/home/grads/tingchih/dataset/DocVQA_task4/competition_dataset/test.json'
+preprocess_dataset = '/home/grads/tingchih/dataset/DocVQA_task4/preprocess_dataset/'
+pages = os.listdir(preprocess_dataset)
+
+
+build_up_nx_KG(val_root, 'val')
+
---
+++ Construction_KG/layout3_text_image_KG_train.py
@@ -0,0 +1,92 @@
+from transformers import AutoProcessor, TFAutoModelForQuestionAnswering
+import tensorflow as tf
+import math
+import os
+import json
+import torch
+import pickle
+from PIL import Image
+
+def build_up_KG(J_FILE):
+    
+    with open(J_FILE) as j:
+        data = json.load(j)
+    
+    for d in range(0, len(data['data'])):
+        extract_info(data['data'][d]['questionId'], data['data'][d]['question'], data['data'][d]['page_ids'])
+
+def extract_info(QID, question, pids):
+    
+    for p in pids:
+        if p in pages:
+            # extract each OCR bboxes, words and image in eact line
+            info = preprocess_dataset + p + '/info.json'
+            ocr = '/home/grads/tingchih/dataset/DocVQA_task4/ocr/' + p + '.json'
+            
+            if os.path.exists(info) == True and os.path.exists(ocr) == True:
+                
+                Q_folder = '/home/grads/tingchih/dataset/DocVQA_task4/competition_dataset/text_image_KG/' + str(QID) + '/'
+                if os.path.exists(Q_folder) == False:
+                    os.mkdir(Q_folder)
+                
+                p_folder = Q_folder + p + '/'
+                if os.path.exists(p_folder) == False:
+                    os.mkdir(p_folder)
+                
+                with open(info) as i:
+                    info_data = json.load(i)
+            
+                with open(ocr) as o:
+                    ocr_data = json.load(o)
+            
+                for k, v in info_data.items():
+                    if int(k) < len(ocr_data['LINE']):
+                        words = v['Text']
+                        image_filename = '/home/grads/tingchih/dataset/DocVQA_task4/' + v['Image']
+                        image = Image.open(image_filename)
+                        original_BBOX = ocr_data['LINE'][int(k)]['Geometry']['BoundingBox'] #dict
+                        normalize = normalize_BBOX(original_BBOX)
+                        
+                        tensor_node = obtain_encoding(image, question, [words], [normalize])
+                        torch_filename = p_folder + str(k) + '.pt'
+                        torch.save(tensor_node, torch_filename)
+    
+    print('[FINISH] ', QID, pids)
+
+def normalize_BBOX(original):
+    # {"BoundingBox": {"Width": , "Height": , "Left": , "Top": }
+    
+    xmin, ymin = original["Left"], original["Top"]
+    xmax, ymax = original["Left"] + original["Width"], original["Top"] + original["Height"]
+    
+    xmin_norm = math.floor(xmin / original["Width"])
+    ymin_norm = math.floor(ymin / original["Height"])
+    xmax_norm = math.floor(xmax / original["Width"])
+    ymax_norm = math.floor(ymax / original["Height"])
+    #print(original)
+    #print(xmin_norm, ymax_norm, xmax_norm, ymin_norm)
+    ans = [xmin_norm, ymax_norm, xmax_norm, ymin_norm]
+    return ans
+
+def obtain_encoding(image, question, words, boxes):
+    
+    encoding = processor(image, question, words, boxes=boxes, return_tensors="tf")
+    start_positions = tf.convert_to_tensor([1])
+    end_positions = tf.convert_to_tensor([3])
+    
+    encoding = processor(image, question, words, boxes=boxes, return_tensors="tf")
+    start_positions = tf.convert_to_tensor([1])
+    end_positions = tf.convert_to_tensor([3])
+    outputs = model(**encoding, start_positions=start_positions, end_positions=end_positions)
+    
+    return outputs
+
+processor = AutoProcessor.from_pretrained("microsoft/layoutlmv3-base", apply_ocr=False)
+model = TFAutoModelForQuestionAnswering.from_pretrained("microsoft/layoutlmv3-base")
+train_root = '/home/grads/tingchih/dataset/DocVQA_task4/competition_dataset/train.json'
+val_root = '/home/grads/tingchih/dataset/DocVQA_task4/competition_dataset/val.json'
+test_root = '/home/grads/tingchih/dataset/DocVQA_task4/competition_dataset/test.json'
+preprocess_dataset = '/home/grads/tingchih/dataset/DocVQA_task4/preprocess_dataset/'
+pages = os.listdir(preprocess_dataset)
+
+build_up_KG(train_root)
